e4daa09bf4e7d59576738cdc66666d989b41961a
linyushan <linyushan@nawang.cn>
2022-07-19 15:52:40 +0800

    Java学习

--- src/com/utils/ArrayUtil.java
+++ src/com/utils/ArrayUtil.java
@@ -1,9 +1,8 @@
 package com.utils;
 
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
+
 
 public class ArrayUtil {
     /**
@@ -12,7 +11,7 @@ public class ArrayUtil {
      * @param arr
      * @return
      */
-    //方法一：需要传入一个Object数组，然后返回去重后的数组
+    //方法一：需要传入一个String数组，然后返回去重后的数组
     public static String[] deduplication(String[] arr) {
         //创建一个集合
         List<String> list = new ArrayList();
@@ -23,9 +22,68 @@ public class ArrayUtil {
                 list.add(arr[i]);
             }
         }
-        //toArray()方法会返回一个包含集合所有元素的Object类型数组
+        //toArray()方法会返回一个包含集合所有元素的String类型数组
         String[] newArr = (String[]) list.toArray(new String[0]);
         return newArr;
+    }
+
+
+    /**
+     * 去重并去除空字符串或者null
+     *
+     * @param arr
+     * @return
+     */
+    public static String[] filterEmptyStr(String[] arr) {
+        //创建一个集合
+        List<String> list = new ArrayList();
+        //遍历数组往集合里存元素
+        for (int i = 0; i < arr.length; i++) {
+            //如果集合里面没有相同的元素才往里存
+            if (!list.contains(arr[i])
+                    && !arr[i].equals("")
+                    && !arr[i].equals(null)
+            ) {
+                list.add(arr[i]);
+            }
+        }
+        //toArray()方法会返回一个包含集合所有元素的String类型数组
+        String[] newArr = (String[]) list.toArray(new String[0]);
+        return newArr;
+    }
+
+    /**
+     * 转为数组
+     *
+     * @param arr
+     * @return
+     */
+    public static String[] getListLongToArr(List<Long> arr) {
+        String arrStr = "";
+        for (Long a : arr) {
+            arrStr += "," + a.toString()
+        }
+        if (arrStr.equals("")) {
+            return new String[0];  //空数组
+        }
+        return arrStr.split(",");
+    }
+
+    /**
+     * 转为数组
+     *
+     * @param arr
+     * @return
+     */
+    public static String[] getListIntegerToArr(List<Integer> arr) {
+        String arrStr = "";
+        for (Long a : arr) {
+            arrStr += "," + a.toString()
+        }
+        if (arrStr.equals("")) {
+            return new String[0];  //空数组
+        }
+        return arrStr.split(",");
     }
 
 
@@ -42,6 +100,17 @@ public class ArrayUtil {
         arr = Arrays.copyOf(arr, strLen1 + strLen2);// 扩容
         System.arraycopy(arr1, 0, arr, strLen1, strLen2);// 将第二个数组与第一个数组合并
         return arr;
+    }
+
+    /**
+     * 判断字符串是否在数组内
+     *
+     * @param arr
+     * @param targetValue
+     * @return
+     */
+    public static boolean contains(String[] arr, String targetValue) {
+        return Arrays.asList(arr).contains(targetValue);
     }
 
 }
